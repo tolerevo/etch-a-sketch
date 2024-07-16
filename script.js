2a62bf5aee1e7cb9622dbc63016f5f940c5530ca
Diego Tolentino <114973390+tolerevo@users.noreply.github.com>
2024-07-15 22:03:21 -0600

    add newBoard func

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 const mainContainer = document.createElement("div");
 mainContainer.classList.add("main-container");
 document.body.appendChild(mainContainer);
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear";
+clearButton.classList.add("button");
+document.body.appendChild(clearButton);
+const newBoardButton = document.createElement("button");
+newBoardButton.textContent = "New Board";
+newBoardButton.classList.add("button");
+document.body.appendChild(newBoardButton);
 const hoveredSquare = document.createElement("input");
 hoveredSquare.type = "checkbox";
 document.body.appendChild(hoveredSquare);
@@ -21,7 +29,39 @@ const changeColor = (event) => {
   event.target.classList.add("changed")
 }
 
+const clearBoard = () => {
+  event.preventDefault();
+  let squares = document.getElementsByClassName("square");
+  for (i=0; i < squares.length; i++) {
+    squares[i].classList.remove("defaultColor", "changed");
+    squares[i].style.removeProperty("background-color");
+  }
+}
+
+const newBoard = () => {
+  event.preventDefault();
+  let userResponse = window.prompt("How many squares would you like? Maximum 100.")
+  if (userResponse > 100) {
+    squareCount = 100;
+  } else {
+    squareCount = userResponse;
+  }
+  renderBoard();
+}
+
+clearButton.addEventListener('click', clearBoard);
+newBoardButton.addEventListener("click", newBoard);
+
+const removeSquares = () => {
+  let squares = document.getElementsByClassName("square");
+  squares = Array.from(squares);
+  for (i = 0; i < squares.length; i++) {
+    squares[i].remove();
+  }
+}
+
 const renderBoard = ()=> {
+  removeSquares();
   for (i=0 ; i < squareCount ** 2 ; i++) {
       let square = document.createElement('div');
       square.addEventListener('mouseover', changeColor);
